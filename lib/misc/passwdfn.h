80fb74290d1baf4af50be554edf5ec562cb26f55
Walter Kleynscheldt <walter.kleynscheldt@hetzner.co.za>
2019-07-14 08:19:00 +0200

    Stop trying to configure and build on Stretch - just install Jessie

--- lib/misc/passwdfn.h
+++
@@ -1,8 +0,0 @@
-#ifndef VMAILMGR__PASSWD_FN__H__
-#define VMAILMGR__PASSWD_FN__H__
-
-#include "mystring/mystring.h"
-
-mystring getpasswd(const char* program);
-
-#endif
--- lib/misc/utoa.h
+++
@@ -1,8 +0,0 @@
-#ifndef VMAILMGR__UTOA__H__
-#define VMAILMGR__UTOA__H__
-
-#include "mystring/mystring.h"
-
-extern mystring utoa(unsigned u);
-
-#endif
